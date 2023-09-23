ecd80a7300522c6109fcc75368c2fcdf75f9d979
Timothy Hart <timothyhart6@gmail.com>
2023-09-22 23:31:35 -0400

    initial commit; added userDistrict input field

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 
 function App() {
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <form action="/userDistrict" method="get">
+          <input type="text" name="streetAddress" value="59 Spruce St"></input>
+          <input type="text" name="zipCode" value="43215"></input>
+          <input type="submit" value="submit"></input>
+        </form>
+
         <a
           className="App-link"
           href="https://reactjs.org"
